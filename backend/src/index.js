36888d176185c2b0eef3dc570628a15deac55c01
itsmeBhavana <bhavana.matavalam@gmail.com>
2024-12-16 23:04:08 -0600

    Added react query for API fetching

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { PORT } from "./config/serverConfig.js";
+import apiRouter from "./routes/index.js";
 
 const app = express();
 
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/api", apiRouter);
 
 app.get("/ping", (req, res) => {
   res.json({ message: "Ping received" });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,22 @@
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import usePing from "./hooks/apis/queries/usePing";
+import { PingComponent } from "./components/atoms/pingComponent";
 
 function App() {
+  const [isVisible, setIsVisible] = useState();
   return (
-    <>
-      Hello
-    </>
-  )
+    <div>
+      <button
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        Toggle
+      </button>
+      {isVisible && <PingComponent />}
+    </div>
+  );
 }
 
-export default App
+export default App;
